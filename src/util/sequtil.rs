2e75597b5c534be1cdd753db4e1476c42504b0ff
Fritz1414213562 <fritz1414213562@gmail.com>
2021-11-18 16:04:08 +0900

    first commit of utility modules

---
+++ src/util/sequtil.rs
@@ -0,0 +1,30 @@
+
+fn convert_aa_3_to_1(aa_3letter: &str) -> Result<char, String> {
+	match aa_3letter {
+		"ALA" => Ok('A'),
+		"CYS" => Ok('C'),
+		"ASP" => Ok('D'),
+		"GLU" => Ok('E'),
+		"PHE" => Ok('F'),
+		"GLY" => Ok('G'),
+		"HIS" => Ok('H'),
+		"ILE" => Ok('I'),
+		"LYS" => Ok('K'),
+		"LEU" => Ok('L'),
+		"MET" => Ok('M'),
+		"ASN" => Ok('N'),
+		"PRO" => Ok('P'),
+		"GLN" => Ok('Q'),
+		"ARG" => Ok('R'),
+		"SER" => Ok('S'),
+		"THR" => Ok('T'),
+		"VAL" => Ok('V'),
+		"TRP" => Ok('W'),
+		"TYR" => Ok('Y'),
+		_     => Err(format!("Unknown residue name, {} exists.", aa_3letter).to_string()),
+	}
+}
+
+pub fn aa_3_to_1(seq_3letter: &Vec<String>) -> Result<Vec<char>, String> {
+	return seq_3letter.iter().map(|aa_3letter| convert_aa_3_to_1(&aa_3letter)).collect()
+}
