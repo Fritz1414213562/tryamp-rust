59d43f9bea744395bcdf32d397a45c3b651a5e62
Fritz1414213562 <fritz1414213562@gmail.com>
2025-02-13 12:11:52 +0900

    fix: add a process to parse the size of unit cell

--- src/dcd/dcd_parser.rs
+++ src/dcd/dcd_parser.rs
@@ -2,7 +2,7 @@ use std::io::Read;
 use std::convert::TryInto;
 use super::dcd_traj::DCDTraj;
 use crate::math::coordinates::Coordinates;
-use super::constant::{DEFAULT_BUFFER_SCALE, COORDINATE_BLOCK_OFFSET, COORDINATE_BLOCK_SCALE};
+use super::constant::{DEFAULT_BUFFER_SCALE, COORDINATE_BLOCK_OFFSET, COORDINATE_BLOCK_SCALE, UNIT_CELL_OFFSET};
 
 
 pub struct DCDParser;
@@ -20,12 +20,18 @@ impl DCDParser {
 		let header_1st  = self.read_block(&mut file)?;
 		let _header_2nd = self.read_block(&mut file)?;
 		let header_3rd  = self.read_block(&mut file)?;
+	
+		let has_unitcell: bool = i32::from_le_bytes(header_1st[44..48].try_into().expect("Can't convert [u8] to [u8;4] for parsing has_unitcell")) != 0;
 
 		let frame_num: i32 = i32::from_le_bytes(header_1st[4..8].try_into().expect("Can't convert [u8] to [u8;4] for parsing 'frame_num'"));
 		let atom_num : i32 = i32::from_le_bytes(header_3rd.try_into().expect("Can't convert [u8] to [u8;4] for parsing 'atom_num'"));
 		let mut traj: Vec<Coordinates<f32>> = Vec::<Coordinates<f32>>::new();
 
-		let coord_block_size: usize = COORDINATE_BLOCK_SCALE * (atom_num as usize * std::mem::size_of::<i32>() + COORDINATE_BLOCK_OFFSET);
+		let coord_block_size: usize = if has_unitcell {
+			COORDINATE_BLOCK_SCALE * (atom_num as usize * std::mem::size_of::<i32>() + COORDINATE_BLOCK_OFFSET) + UNIT_CELL_OFFSET
+		} else {
+			COORDINATE_BLOCK_SCALE * (atom_num as usize * std::mem::size_of::<i32>() + COORDINATE_BLOCK_OFFSET)
+		};
 		let buffer_capacity: usize = DEFAULT_BUFFER_SCALE * coord_block_size;
 		let mut buffer: Vec<u8> = vec![0; buffer_capacity];
 
@@ -33,7 +39,7 @@ impl DCDParser {
 			match file.read(&mut buffer) {
 				Ok(0) => break,
 				Ok(buffer_size) => {
-					match buffer[..buffer_size].chunks(coord_block_size).map(|mut xyz_bytes| self.read_xyz(&mut xyz_bytes)).collect::<Result<Vec<Coordinates<f32>>, String>>() {
+					match buffer[..buffer_size].chunks(coord_block_size).map(|mut xyz_bytes| self.read_xyz(&mut xyz_bytes, has_unitcell)).collect::<Result<Vec<Coordinates<f32>>, String>>() {
 						Ok(part_traj) => traj.extend(part_traj),
 						Err(err) => return Err(err),
 					}
@@ -52,7 +58,8 @@ impl DCDParser {
 	}
 
 
-	fn read_xyz<R: Read>(&self, bytes: &mut R) -> Result<Coordinates<f32>, String> {
+	fn read_xyz<R: Read>(&self, bytes: &mut R, has_unitcell: bool) -> Result<Coordinates<f32>, String> {
+		if has_unitcell { let _ = self.read_block(bytes)?; }
 		let coord_x_bytes = self.read_block(bytes)?;
 		let coord_xs: Vec<f32> = coord_x_bytes.chunks(4).map(|coord_x_byte| f32::from_le_bytes(coord_x_byte.try_into().expect("Can't convert [u8] to [u8; 4] for parsing coordinate_x"))).collect();
 		let coord_y_bytes = self.read_block(bytes)?;
