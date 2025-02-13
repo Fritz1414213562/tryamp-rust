b1ed559882ae826f263ca3ffb70bfdc7c20d20cb
Fritz1414213562 <fritz1414213562@gmail.com>
2025-02-13 12:10:26 +0900

    fix: define a constant for unit cell

--- src/dcd/constant.rs
+++ src/dcd/constant.rs
@@ -1,6 +1,7 @@
 pub const DEFAULT_BUFFER_SCALE   : usize = 10; // read 10 frames at once
 pub const COORDINATE_BLOCK_OFFSET: usize = 8;  // size of block-size description (i32 x 2)
 pub const COORDINATE_BLOCK_SCALE : usize = 3;  // 3 dimensions, (x, y, z)
+pub const UNIT_CELL_OFFSET       : usize = 56; // size of block-size + parameters of unit cell (i32 x 2 + f64 x 6);
 
 pub const DCD_SECOND_BLOCK: &[u8] = b"======================= Written by DCD Writer in try-amp =======================";
 pub const DCD_SECOND_BLOCKSIZE: i32 = (DCD_SECOND_BLOCK.len() + 4) as i32;
